1759dfea9d86af41197f78355c3eb3d0bec89a49
Galveill <97130439+Galveill@users.noreply.github.com>
2024-11-08 18:32:30 +0100

    Añadido el nivel de amenaza.

---
+++ src/enums/EAmenaza.java
@@ -0,0 +1,30 @@
+package enums;
+
+/**
+ * El nivel de amenaza de una zona. Determina el número de enemigos que hay.
+ * @author Adrián.
+ */
+public enum EAmenaza {
+	PACIFICO(0),
+	BAJO(1),
+	MEDIO(2),
+	DIFICIL(3),
+	AVANZADO(4);
+
+	/** El nivel representativo */
+	private int lvl;
+
+	/**
+	 * Constructor básico para darle valor.
+	 * @param lvl El nivel representativo.
+	 */
+	private EAmenaza(int lvl)
+	{
+		this.lvl = lvl;
+	}
+
+	public String toString()
+	{
+		return this.lvl + "";
+	}
+}
