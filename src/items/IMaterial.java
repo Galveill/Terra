711689786e91d6d7cf1ed1c1157750be729495ce
Adrián <97130439+Galveill@users.noreply.github.com>
2024-11-14 15:26:55 +0100

    Inicio de la jerarquía de interfaces de los objetos.

---
+++ src/items/IMaterial.java
@@ -0,0 +1,9 @@
+package items;
+
+/**
+ * El elemento es un material del sistema.
+ * @author Adrián.
+ */
+public interface IMaterial extends IObjeto {
+
+}
---
+++ src/items/IObjeto.java
@@ -0,0 +1,9 @@
+package items;
+
+/**
+ * El elemento es un objeto del sistema.
+ * @author Adrián.
+ */
+public interface IObjeto {
+
+}
