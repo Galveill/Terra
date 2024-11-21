d83737ddd32748535262ea5cfe9457277fc9a352
Adrián <97130439+Galveill@users.noreply.github.com>
2024-11-21 22:59:02 +0100

    Tipos de materiales.

---
+++ src/items/materials/EMaterialTipo.java
@@ -0,0 +1,21 @@
+package items.materials;
+
+/**
+ * Tipos de materiales.
+ * @author Adrián.
+ */
+public enum EMaterialTipo {
+	MINERAL,
+	MADERA,
+	PIEDRA,
+	ARCILLA,
+
+	CUERO,
+	PLUMA,
+
+	CERA,
+
+	CORAL,
+	PERLA,
+	NACAR;
+}
