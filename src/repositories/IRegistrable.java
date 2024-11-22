658a19f2ea8ea262ffdef7ab64f204b60b91ac13
Adrián <97130439+Galveill@users.noreply.github.com>
2024-11-22 22:13:33 +0100

    Añadidas las bases para los repositorios basados en código.

---
+++ src/repositories/IRegistrable.java
@@ -0,0 +1,13 @@
+package repositories;
+
+/**
+ * Da la capacidad de ser registrado en un repositorio.
+ * @author Adrián.
+ */
+public interface IRegistrable {
+
+	/**
+	 * @return Devuelve el código identificador.
+	 */
+	public String getCode();
+}
