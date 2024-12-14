4f61f27e9caf93cf33aef3b6f7c3d1b72f1f7430
Adrián <97130439+Galveill@users.noreply.github.com>
2024-12-14 20:28:24 +0100

    Métodos auxiliares de la magia.

--- src/data/magic/MagiaDatos.java
+++ src/data/magic/MagiaDatos.java
@@ -6,6 +6,67 @@ package data.magic;
  */
 public class MagiaDatos {
 
-	//TODO Implementar.
-	//TODO getWeakElement para obtener el elemento que le hace más daño.
+	/**
+	 * Indica el elemento que le hace más daño al elemento dado.
+	 * @param elem El elemento a consultar.
+	 * @return El elemento al que es débil.
+	 */
+	public static EElemento getWeakElement(EElemento elem)
+	{
+		switch (elem) {
+			case AGUA:
+				return EElemento.FUEGO;
+			case FUEGO:
+				return EElemento.AGUA;
+			case TIERRA:
+				return EElemento.VIENTO;
+			case VIENTO:
+				return EElemento.TIERRA;
+			case LUZ:
+				return EElemento.OSCURIDAD;
+			case OSCURIDAD:
+				return EElemento.LUZ;
+			case ETER:
+				return EElemento.ETER;
+			case MAGMA:
+				return EElemento.HIELO;
+			case HIELO:
+				return EElemento.ELECTRICIDAD;
+			case ELECTRICIDAD:
+				return EElemento.NATURA;
+			case NATURA:
+				return EElemento.MAGMA;
+		}
+		return null;
+	}
+
+	/**
+	 * Indica si los elementos son afines.
+	 * @param elemA Primer elemento.
+	 * @param elemB Segundo elemento.
+	 * @return True si son afines, false en caso contrario.
+	 */
+	public static boolean related(EElemento elemA, EElemento elemB)
+	{
+		switch (elemA) {
+			case MAGMA:
+				return elemB == EElemento.TIERRA || elemB == EElemento.FUEGO;
+			case NATURA:
+				return elemB == EElemento.TIERRA || elemB == EElemento.AGUA;
+			case HIELO:
+				return elemB == EElemento.AGUA || elemB == EElemento.VIENTO;
+			case ELECTRICIDAD:
+				return elemB == EElemento.FUEGO || elemB == EElemento.VIENTO;
+			case TIERRA:
+				return elemB == EElemento.MAGMA || elemB == EElemento.NATURA;
+			case FUEGO:
+				return elemB == EElemento.MAGMA || elemB == EElemento.ELECTRICIDAD;
+			case VIENTO:
+				return elemB == EElemento.HIELO || elemB == EElemento.ELECTRICIDAD;
+			case AGUA:
+				return elemB == EElemento.HIELO || elemB == EElemento.NATURA;
+			default:
+				return false;
+		}
+	}
 }
